722b318fe98b518e2571ba7323f7bec2fec6c7c0
Steven Green <dsshard@gmail.com>
2023-01-21 04:22:21 +0300

    Fixed trim

--- dist/strings/trim-by-size.js
+++ dist/strings/trim-by-size.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.trimBySize = void 0;
 function trimBySize(str, max) {
+    if (!str)
+        return str;
     return str.length > max ? str.substring(0, max) : str;
 }
 exports.trimBySize = trimBySize;
