4f12de1e636569b5045d38a2a7c83838fb5ace09
Steven Green <dsshard@gmail.com>
2024-01-11 21:52:52 +0300

    Rebuild

--- src/data/array-search-in-by-field.ts
+++ src/data/array-search-in-by-field.ts
@@ -1,10 +1,16 @@
 type FieldExtractor<T> = (sub: T) => string
 
+function escapeRegex(string: string) {
+  const reRegExpChar = /[\\^$.*+?()[\]{}|]/g
+  const reHasRegExpChar = RegExp(reRegExpChar.source)
+  return string && reHasRegExpChar.test(string) ? string.replace(reRegExpChar, '\\$&') : string || ''
+}
+
 export function searchArrayFilter<T>(array: T[], searchString: string, _fields: (keyof T)[] | FieldExtractor<T>) {
   if (!searchString) {
     return array
   }
-  const reg = new RegExp(encodeURI(String(searchString).toLowerCase()), 'ig')
+  const reg = new RegExp(escapeRegex(searchString).toLowerCase(), 'ig')
   return array.filter((sub: T) => {
     const fields = (typeof _fields === 'function' ? _fields(sub) : _fields.map((field) => sub[field])) as string[]
 
